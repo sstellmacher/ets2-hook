220ce587e9f1aaf49fadf60edbb482d24046e839
sstellmacher <sstellmacher-dev@gmx.net>
2022-04-11 07:23:39 +0200

    Added SDK Client implementation

--- Ets2Client/Telemetry/DefaultEts2TelemetryProvider.cs
+++ Ets2Client/Telemetry/DefaultEts2TelemetryProvider.cs
@@ -1,16 +1,44 @@
 ﻿using Ets2SdkClient;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Ets2Client.Telemetry
 {
     public class DefaultEts2TelemetryProvider : IEts2TelemetryProvider
     {
-        // todo
+        private readonly Ets2SdkTelemetry telemetry = new Ets2SdkTelemetry();
+        private readonly ILogger<DefaultEts2TelemetryProvider> logger;
+
+        private Ets2Telemetry telemetryData;
+        private bool newTimestamp = false;
+
+        public DefaultEts2TelemetryProvider(ILogger<DefaultEts2TelemetryProvider> logger)
+        {
+            this.logger = logger;
+            telemetry.Data += Telemetry_Data;
+        }
+
+        private void Telemetry_Data(Ets2Telemetry telemetryData, bool newTimestamp)
+        {
+            this.telemetryData = telemetryData;
+            this.newTimestamp = newTimestamp;
+        }
 
         public async Task<Ets2Telemetry> GetTelemetryInfoAsync()
         {
-            await Task.Yield();
-            return null;
+            if (telemetry.Error != null)
+                throw telemetry.Error;
+
+            logger.LogInformation("Waiting for new data...");
+            while (!newTimestamp)
+            {
+                await Task.Yield();
+            }
+
+            newTimestamp = false;
+            logger.LogInformation("Data received.");
+
+            return telemetryData;
         }
     }
 }
